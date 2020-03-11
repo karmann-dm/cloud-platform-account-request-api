0db614d4760c4441f35593ab1dbdffb507da80a1
karmann-dm <karmanno@gmail.com>
2020-03-11 14:54:17 +0300

    Initial

---
+++ src/main/java/com/karmanno/cloudaccountrequestsapi/CloudAccountRequestsApiApplication.java
@@ -0,0 +1,13 @@
+package com.karmanno.cloudaccountrequestsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAccountRequestsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAccountRequestsApiApplication.class, args);
+    }
+
+}
